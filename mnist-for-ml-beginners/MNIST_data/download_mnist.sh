6f68d50cc3ca855903aa77d2f22a3c034873b398
Karel Ha <mathemage@gmail.com>
2017-03-22 10:39:37 +0100

    Use script directory for paths in test_build.sh and download_mnist.sh
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- mnist-for-ml-beginners/MNIST_data/download_mnist.sh
+++ mnist-for-ml-beginners/MNIST_data/download_mnist.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/bash
 files="t10k-images-idx3-ubyte.gz t10k-labels-idx1-ubyte.gz train-images-idx3-ubyte.gz train-labels-idx1-ubyte.gz"
 url="https://web.archive.org/web/20150906081542/http://yann.lecun.com/exdb/mnist/"
+script_dir=`dirname $0`
+echo "Directory of this script is $script_dir"
 
+(
+cd $script_dir
 for file in $files; do
   command="wget $url$file"
   $command
 done
+)
--- test/test_builds.sh
+++ test/test_builds.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
+script_dir=`dirname $0`
+echo "Directory of this script is $script_dir"
 
-(
-cd ../getting-started/
-python3 linear-regression-tf-core-version.py
-python3 linear-regression-tf-contrib-version.py
+python3 $script_dir/../getting-started/linear-regression-tf-core-version.py
+python3 $script_dir/../getting-started/linear-regression-tf-contrib-version.py
 
-cd ../mnist-for-ml-beginners/
-python3 mnist-softmax.py
-)
+python3 $script_dir/../mnist-for-ml-beginners/mnist-softmax.py
