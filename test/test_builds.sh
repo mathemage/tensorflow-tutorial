7945e58f77665f1390e85f65e90bc488decfdfce
Karel Ha <mathemage@gmail.com>
2017-03-22 17:58:24 +0100

    Add test of linear-regression-custom.py to travis
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- test/test_builds.sh
+++ test/test_builds.sh
@@ -5,6 +5,6 @@ script_dir=`dirname $0`
 echo "Directory of script $0 is $script_dir"
 
 python3 $script_dir/../getting-started/linear-regression-tf-core-version.py
-python3 $script_dir/../getting-started/linear-regression-tf-contrib-version.py
+python3 $script_dir/../getting-started/linear-regression-custom.py
 
 python3 $script_dir/../mnist-for-ml-beginners/mnist-softmax.py
